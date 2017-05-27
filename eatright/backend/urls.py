c0bc37468dea61550ac0c4ca22a93833fff99cd8
himanshu1410@live.com <Himanshu1410@live.com>
2017-05-27 10:46:30 -0700

    Removed unnecessary backend urls

--- eatright/backend/urls.py
+++ eatright/backend/urls.py
@@ -2,9 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    #url(r'^$', views.index, name='index'),
-    #url(r'^backend/$', views.backend_list),
     url(r'^restaurant/$', views.get_restaurant_list),
     url(r'^menu_info/$', views.get_menu_info),
-    #:url(r'^backend/(?P<pk>[0-9]+)/$', views.backend_detail),
 ]
